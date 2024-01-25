be61f0f47e9cfca035b0546c4d1f82a9bc5d598b
Christopher <chrisnieves60@gmail.com>
2024-01-25 01:30:04 -0500

    fix .env

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     env: {
       AWS_REGION: process.env.AWS_REGION,
       AWS_COGNITO_IDENTITY_POOL_ID: process.env.AWS_COGNITO_IDENTITY_POOL_ID,
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+
     },
     // ... any other config options you have
   };
