59eaed490006b834101a9441e4e7997473f7f9bd
Christopher <chrisnieves60@gmail.com>
2024-01-31 16:35:13 -0500

    added credentials

--- awsConfig.js
+++ awsConfig.js
@@ -1,5 +1,9 @@
 import AWS from 'aws-sdk';
 // Initialize the Amazon Cognito credentials provider
-AWS.config.region = "us-east-2"; // Region
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'your-region' // Example: 'us-east-1'
+  });
 // Now the AWS SDK is configured and ready to use
 export default AWS;
