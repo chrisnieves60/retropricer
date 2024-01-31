fc18c5241ebc20a6e035f3a341790a42afd8ef40
Christopher <chrisnieves60@gmail.com>
2024-01-31 16:52:17 -0500

    fixed region setting for aws

--- awsConfig.js
+++ awsConfig.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: 'your-region' // Example: 'us-east-1'
+    region: process.env.AWS_REGION // Example: 'us-east-1'
   });
 // Now the AWS SDK is configured and ready to use
 export default AWS;
